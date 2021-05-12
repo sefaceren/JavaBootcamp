46d0653d74577c87361eb4608b8a779a08246797
sefaceren <h.sefaceren@gmail.com>
2021-05-12 17:25:14 +0300

    1.Odev üzerinde çalışıyorum.

---
+++ 1.Gun/1.Odev/datatypes/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    double sayi = 12.5;
+	    sayi = 13;
+
+	    boolean dogruMu = true;
+	    char karakter = 'A';
+    }
+}
---
+++ 1.Gun/1.Odev/variables/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int ogrenciSayisi = 12;
+        String mesaj = "Öğrenci Sayısı : ";
+	    System.out.println( mesaj + ogrenciSayisi);
+        // değişken içeriğini her yerden manuel olarak değiştirmemek için değişkenlere atıyoruz ve tek bir yerden
+        // değiştirerek kolaylık sağlıyoruz. ( reusability )
+    }
+}
