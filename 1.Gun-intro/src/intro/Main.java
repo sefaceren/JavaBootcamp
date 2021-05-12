19985af6fe12c7135fe47473ac173fce0c2b890f
sefaceren <h.sefaceren@gmail.com>
2021-05-12 15:23:47 +0300

    initial commit

---
+++ 1.Gun-intro/src/intro/Main.java
@@ -0,0 +1,56 @@
+package intro;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//camelCase
+		//Don't repeat yourself
+		String internetSubeButonu = "Ýnternet Þubesi";
+		double dolarDun = 8.20;
+		double dolarBugun = 8.20;
+		int vade = 36;
+		boolean dustuMu = false;
+		
+		System.out.println(internetSubeButonu);
+		
+		if(dolarBugun<dolarDun) {
+			System.out.println("Dolar Düþtü Resmi");
+		}else if (dolarBugun>dolarDun) {
+			System.out.println("Dolar Yükseldi Resmi");
+		}else {
+			System.out.println("Dolar Eþittir Resmi");
+		}
+		
+		String kredi1 = "Hýzlý Kredi";
+		String kredi2 = "Tarým Kredi";
+		String kredi3 = "Konut Kredi";
+		String kredi4 = "Taþýt Kredi";
+		String kredi5 = "Motor Kredi";
+		
+		System.out.println(kredi1);
+		System.out.println(kredi2);
+		System.out.println(kredi3);
+		System.out.println(kredi4);
+		System.out.println(kredi5);
+		
+		String[] krediler = {
+			"Hýzlý Kredi",
+			"Tarým Kredi",
+			"Konut Kredi",
+			"Taþýt Kredi",
+			"Motor Kredi"
+		};
+		
+		for(String kredi: krediler) {
+			System.out.println(kredi);
+		}
+		
+		for(int i = 0;i< krediler.length ;i++) {
+			System.out.println(krediler[i]);
+		}
+		
+
+	}
+
+}
